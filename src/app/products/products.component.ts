455c90b2661d7c63c659024a70c5fba27d7550e6
haseeAmarathunga <haseeamarathunga@gmail.com>
2019-12-28 16:16:53 +0530

    Update Product list view

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {CategoryService} from '../services/category.service';
 import {MatDialog, MatDialogConfig} from '@angular/material';
 import {AddCartComponent} from './add-cart/add-cart.component';
+import {ProductService} from '../services/product.service';
+import {Product} from '../model/product';
 
 @Component({
   selector: 'app-products',
@@ -11,13 +13,19 @@ import {AddCartComponent} from './add-cart/add-cart.component';
 export class ProductsComponent implements OnInit {
 
   constructor(private categoryService: CategoryService,
+              private productService: ProductService,
               private dialog: MatDialog) { }
-
-  products = [1, 2, 3, 4, 5, 6];
   categories;
+  products;
+  selectedCategory = 'all';
+  selectedProducts;
   async ngOnInit() {
     this.categoryService.loadCategories().subscribe(res => {
       this.categories = res;
+    });
+    this.productService.loadProducts().subscribe(res => {
+      this.products = res;
+      this.selectedProducts = this.products;
     });
   }
   addCart(row) {
@@ -29,6 +37,35 @@ export class ProductsComponent implements OnInit {
     this.dialog.open(AddCartComponent, dialogConfig).afterClosed().subscribe(result => {
       console.log('Closed');
     });
+  }
+  selectCategory(category) {
+    if (category) {
+      this.selectedCategory = category.code;
+      this.selectedProducts = this.filterProductsByCatCode(this.selectedCategory);
+    } else {
+      this.selectedCategory = 'all';
+      this.selectedProducts = this.products;
+    }
+  }
+  filterProductsByCatCode(catCode) {
+    const productArr = [];
+    this.products.forEach(product => {
+      if (product.categoryCode === catCode) {
+        productArr.push(product);
+      }
+    });
+    return productArr;
+  }
+  getLengthFromCatCode(code) {
+    if (this.products) {
+      let i = 0;
+      this.products.forEach(product => {
+        if (product.categoryCode === code) {
+          i++;
+        }
+      });
+      return i;
+    }
   }
 
 }
